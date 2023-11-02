68e6d5fe632c8860e35e4b223bcf95eeb56dd17f
minhee231 <goominhee123@gmail.com>
2023-11-02 18:26:38 +0900

    2023-11-02

---
+++ 2023-11-02/cvVer2.py
@@ -0,0 +1,20 @@
+
+import cv2
+
+def read_qrcode_cv2(open_img):
+    detector = cv2.QRCodeDetector()
+    data, bbox, _ = detector.detectAndDecode(open_img)
+    if data:
+        print(data)
+        print(bbox)
+
+        lefttop = (int(bbox[0][0][0]), int(bbox[0][0][0]))
+        righttop = (int(bbox[0][2][0]), int(bbox[0][2][1]))
+
+        
+# filepath = "C:/Users/goomi/OneDrive/바탕 화면/작업/Desk/창남쌤파이썬방과후/2023-11-02/qr_wifi2.png"
+# filepath = "./qr_wifi2.png"
+# filepath = 'https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F0156e6cd-5919-408d-a3da-5c9efa4be01e%2F0266bf58-74da-4dce-9d32-8c6328da03ad%2Fqr_wifi2.png?table=block&id=c1801a43-2033-4807-acc5-e3df7b0ebfb1&spaceId=0156e6cd-5919-408d-a3da-5c9efa4be01e&width=2000&userId=ad050ec8-fa93-4db8-9dbc-7c288cfa1aba&cache=v2'
+filepath = 'https://imgur.com/hQYBlH9.png'
+cv_img1=cv2.imread(filepath)
+read_qrcode_cv2(cv_img1)
---
+++ 2023-11-02/qrcd.py
@@ -0,0 +1,4 @@
+import qrcode
+
+img = qrcode.make("그쪽도 자겁치까를 아세요?? 왁왁 우왁 그쪽도 자겁치까를 하는지이이이? 부어엉궈 우부우부억 부와")
+img.save('DoYouKnwoWorkHeal.png')
